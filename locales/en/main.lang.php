7012615683a3c62d3dbb4da1e094140047f0f507
franck <carnet.franck.paul@gmail.com>
2023-04-12 14:57:02 +0200

    l10n and transifex config

---
+++ locales/en/main.lang.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['YASH'] = '';
+L10n::$locales['Yet Another Syntax Highlighter'] = '';
+L10n::$locales['Highlighted Code'] = '';
+L10n::$locales['Plain Text'] = '';
+L10n::$locales['AppleScript'] = '';
+L10n::$locales['ActionScript3'] = '';
+L10n::$locales['Bash/shell'] = '';
+L10n::$locales['ColdFusion'] = '';
+L10n::$locales['C#'] = '';
+L10n::$locales['C/C++'] = '';
+L10n::$locales['CSS'] = '';
+L10n::$locales['Delphi'] = '';
+L10n::$locales['Diff/Patch'] = '';
+L10n::$locales['Erlang'] = '';
+L10n::$locales['Groovy'] = '';
+L10n::$locales['Haxe'] = '';
+L10n::$locales['Javascript/JSON'] = '';
+L10n::$locales['Java'] = '';
+L10n::$locales['JavaFX'] = '';
+L10n::$locales['Perl'] = '';
+L10n::$locales['PHP'] = '';
+L10n::$locales['PowerShell'] = '';
+L10n::$locales['Python'] = '';
+L10n::$locales['Ruby'] = '';
+L10n::$locales['SASS'] = '';
+L10n::$locales['Scala'] = '';
+L10n::$locales['SQL'] = '';
+L10n::$locales['Tap'] = '';
+L10n::$locales['TypeScript'] = '';
+L10n::$locales['Visual Basic'] = '';
+L10n::$locales['XML/XSLT/XHTML/HTML'] = '';
+L10n::$locales['Yaml'] = '';
+L10n::$locales['YASH - Syntax Selector'] = '';
+L10n::$locales['Select the primary syntax of your code snippet.'] = '';
+L10n::$locales['Cancel'] = '';
+L10n::$locales['Ok'] = '';
+L10n::$locales['Default'] = '';
+L10n::$locales['Django'] = '';
+L10n::$locales['Eclipse'] = '';
+L10n::$locales['Emacs'] = '';
+L10n::$locales['Fade to gray'] = '';
+L10n::$locales['Material'] = '';
+L10n::$locales['MD Ultra'] = '';
+L10n::$locales['Midnight'] = '';
+L10n::$locales['RDark'] = '';
+L10n::$locales['Solarized Dark'] = '';
+L10n::$locales['Solarized Light'] = '';
+L10n::$locales['Tomorrow Night'] = '';
+L10n::$locales['Enable YASH'] = '';
+L10n::$locales['Presentation'] = '';
+L10n::$locales['Theme:'] = '';
+L10n::$locales['Use custom CSS:'] = '';
+L10n::$locales['You can use a custom CSS by providing its location.'] = '';
+L10n::$locales['A location beginning with a / is treated as absolute, else it is treated as relative to the blog\'s current theme URL'] = '';
+L10n::$locales['Hide gutter with line numbers'] = '';
+L10n::$locales['Options'] = '';
+L10n::$locales['SyntaxeHL compatibility mode'] = '';
+L10n::$locales['All SyntaxeHL languages is not supported by Yash (see documentation).'] = '';
+L10n::$locales['Will be applied on future edition of posts containing SyntaxeHL macros (///[language]…///).'] = '';
