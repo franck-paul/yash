7b0149a0d5279a85cef737221202d1bc4df218f2
franck <carnet.franck.paul@gmail.com>
2023-06-06 14:01:57 +0200

    Oups

--- src/My.php
+++ src/My.php
@@ -195,7 +195,7 @@ class My
         // Comment second line if you only have one icon.svg for both mode
         return [
             urldecode(dcPage::getPF(self::id() . '/icon.svg')),         // Light (or both) mode(s)
-            urldecode(dcPage::getPF(self::id() . '/icon-dark.svg')),    // Dark mode
+            // urldecode(dcPage::getPF(self::id() . '/icon-dark.svg')),    // Dark mode
         ];
     }
 
