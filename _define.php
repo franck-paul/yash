8702cc219b11023fd7384151535b54b44421e7ef
franck <carnet.franck.paul@gmail.com>
2023-08-26 12:57:38 +0200

    New release 4.0

--- _define.php
+++ _define.php
@@ -15,7 +15,7 @@ $this->registerModule(
     'YASH',
     'Yet Another Syntax Highlighter',
     'Pep and contributors',
-    '3.3.1',
+    '4.0',
     [
         'requires'    => [['core', '2.27'], ['php', '8.1']],
         'permissions' => dcCore::app()->auth->makePermissions([
