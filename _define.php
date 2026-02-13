9eb1c1f7d812e2bf4e55fec49c23fe4cb1a90a7e
franck <carnet.franck.paul@gmail.com>
2026-02-13 10:37:38 +0100

    New release 8.0

--- _define.php
+++ _define.php
@@ -16,9 +16,9 @@ $this->registerModule(
     'YASH',
     'Yet Another Syntax Highlighter',
     'Pep and contributors',
-    '7.5',
+    '8.0',
     [
-        'date'        => '2025-12-12T12:53:21+0100',
+        'date'        => '2026-02-13T10:37:38+0100',
         'requires'    => [['core', '2.37']],
         'permissions' => 'My',
         'priority'    => 1010,  // Must be higher than dcLegacyEditor/dcCKEditor priority (ie 1000)
